920a8fde4e329dcf9c58ee1878f383da2a04b6e6
Jon Gainza <124215384+jongainza@users.noreply.github.com>
2026-03-16 13:28:59 -0700

    add function getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+
+function getComputerChoice(){
+
+let computerChoice = Math.floor(Math.random() * 3) + 1;
+switch (computerChoice){
+ case 1:
+  console.log('Rock')
+  return 'Rock'
+ case 2:
+   console.log("Paper");
+   return 'Paper'
+ case 3: 
+   console.log("Scissors");
+   return 'Scissors'
+}
+
+
+
+}
+getComputerChoice()
